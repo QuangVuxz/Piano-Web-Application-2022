444ac4b5c6bd5294c2ead62e14206f6d837515d9
Bi11-204 <101236910+Bi11-204@users.noreply.github.com>
2023-01-08 17:39:15 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+DOCTYPE html>
+<html>
+  <head>
+    <title>Welcome to Piano Course</title>
+    <link rel = "icon" href = 
+  "https://img.freepik.com/free-vector/music-design-yellow-illustration_24877-49373.jpg?auto=format&h=200" 
+          type = "image/x-icon">
+    <link rel="stylesheet" href="styleIndex.css">
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="index.php" style="color:red">Home</a></li>
+      </ul>
+    </nav>
+    <div class="container">
+      <!-- Add a heading to the page -->
+      <h1>WELCOME TO PIANOONLINE.COM</h1>
+      <h2>Free piano teaching website for those who have never played or are planning to learn</h2>
+      <h3>WHY SHOULD YOU LEARN PIANO ONLINE?</h3>
+      <!-- Add some content to the page -->
+      <table>
+      <tr>
+      <th>NO MUSICAL THEORY</th>
+      <th>DOES NOT TAKE LONG TIME TO LEARN</th>
+      <th>NO DIFFICULTY</th>
+      <th>100% FREE</th>
+      <th>LEARN ANYTIME  ANYWHERE</th>
+      </tr>
+      <tr>
+      <td>This learning method is based on the traditional way of learning, which is difficult for the teacher but easier to understand and more inspiring for the learners. You don't need to know any musical note to be able to play in a very short time</td>
+      <td>The lessons of the piano class are very simple and easy to understand. You only need 1 month of online learning to be able to master the piano, play some songs - with clear finished products - so you won't get discouraged quickly.</td>
+      <td>The curriculum is taught by hand, not using advanced theories. And the lectures are as easy to understand as possible. Therefore, even if you are not a musically gifted person, it is easy to absorb and practice.</td>
+      <td>All lessons are free for you</td>
+      <td>Just need a phone with internet access and go to Pianoonline.com website, you can study at any time.</td>
+      </tr>
+      </table>
+      
+      <!-- Add a call-to-action button -->
+      <p>
+        <a href="login.php" class="cta-btn">Try Now</a>
+      </p>
+      <h2>FAQ</h2>
+      <p><b>1. IS THIS WEBSITE FREE?</b></p>
+      <p><i>All lessons on the website are free for young people who are passionate about learning and learning about the piano</i></p>
+      <p><b>2. IS IT DIFFICULT TO LEARN THE PIANO BY YOURSELF?</b></p>
+      <p><i>Like other musical instruments, it is difficult or easy to learn depending on the learner's aptitude and acquisition. However, on the website pianoonline.com, we have researched and come up with the most effective textbooks and methods of communication, guaranteed to be suitable for you.</i></p>
+      <p><b>3.I DON'T KNOW ABOUT MUSIC, CAN I LEARN?</b></p>
+      <p><i>Absolutely possible.The textbooks and lectures in the application have been studied by lecturers for more than 10 years and have been effectively applied. This special teaching and learning method helps you without having to have knowledge of music theory to still be able to study well and play the songs you like well after only a short time of learning according to the route outlined by the application.</i></p>
+      <h4>PIANOONLINE.COM - PIANO FOR BEGINNERS</h4>
+    </div>
+  </body>
+</html>
